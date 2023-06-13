ee0a86573ed6527181d30229ca2feb5c3f83ae6f
Ankit Jain <ajain5@egencia.com>
2023-06-13 21:57:50 +0530

    solution

---
+++ src/main/kotlin/exercises/solution/Collection.kt
@@ -0,0 +1,20 @@
+package exercises
+
+/*
+*
+* You need to filter the short words based on given length and remove the basic articles from the final result.
+* Also make sure no words repeat twice in the result.
+*
+ */
+fun main() {
+    val words = "We are learning Kotlin master class in the training room and We will make sure our team wins this round".split(" ")
+    val articles = setOf("a", "A", "an", "An", "the", "The")
+    val minLength = 4
+    val result = shortNonRepeatingWords(words, articles, minLength)
+
+    println(result)
+    println(result.size)
+}
+
+fun shortNonRepeatingWords(words: List<String>, articles: Set<String>, minLength: Int) =
+    words.filter { it.length < 4 }.toSet().minus(articles)
